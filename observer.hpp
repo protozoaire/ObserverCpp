3c41bc37cbc7518335ccffc7b45de59a09954566
Vivien Dubois <vivien@voidetoile.fr>
2022-01-11 17:28:27 +0100

    SubjectEvents and Event tags

--- observer.hpp
+++ observer.hpp
@@ -5,13 +5,180 @@
 
 
 #include <cstddef>
+#include <type_traits>
+#include <memory>
+
+
+//  An Implementation of the Observer Pattern;
+//
+//  USAGE:
+//  
+//  - One creates an events's set, e.g.
+//      
+//      struct Event0 { ... };
+//      struct Event1 { ... };
+//      ...
+//
+//      using events_t = SubjectEvents<Event0,Event1,...>;
 
 
 namespace Observer {
 
 
+template <typename ... Events>
+struct SubjectEvents;
+
+template <typename E, typename _SubjectEvents>
+struct Event;
 
 
+
+//
+//
+//
+
+
+template <typename E, typename ... List>
+struct _Count;
+
+template <typename E, typename H, typename ... Rs>
+struct _Count<E,H,Rs...>
+{
+    static constexpr size_t value = _Count<E,Rs...>::value;
+};
+
+template <typename E, typename ... Rs>
+struct _Count<E,E,Rs...>
+{
+    static constexpr size_t value = 1+_Count<E,Rs...>::value;
+};
+
+template <typename E>
+struct _Count<E>
+{
+    static constexpr size_t value = 0;
+};
+
+template <typename ... >
+struct _EachOnce
+{
+    static constexpr bool value = true;
+};
+
+template <typename H, typename ... Rs>
+struct _EachOnce<H,Rs...>
+{
+    static constexpr bool value = (_Count<H,Rs...>::value == 0) && _EachOnce<Rs...>::value;
+};
+
+template <>
+struct _EachOnce<>
+{
+    static constexpr bool value = true;
+};
+
+template <template <typename> class Pred, typename ... >
+struct _ForEach;
+
+template <template <typename> class Pred, typename H, typename ... Rs>
+struct _ForEach<Pred,H,Rs...>
+{
+    static constexpr bool value = Pred<H>::value && _ForEach<Pred,Rs...>::value;
+};
+
+template <template <typename> class Pred>
+struct _ForEach<Pred>
+{
+    static constexpr bool value = true;
+};
+
+template <typename T>
+struct _IsDefined
+{
+    template <typename _T = T, size_t = sizeof(_T)>
+    static std::true_type test(int);
+
+    static std::false_type test(...);
+
+    static constexpr bool value = decltype(test(0))::value;
+};
+
+template <typename ... Events>
+struct SubjectEvents
+{
+    
+    static constexpr bool AssertNoDuplicate() {
+        //each event must appear once in the list;
+        return _EachOnce<Events...>::value;
+    }
+
+    static constexpr bool AssertAllDefined() {
+        //each event must be defined;
+        return _ForEach<_IsDefined,Events...>::value;
+    }
+
+};
+
+
+//
+//
+//
+
+
+template <typename E, typename _SubjectEvents>
+struct IsSupportedEvent;
+
+template <typename E, typename ... Events>
+struct IsSupportedEvent< E, SubjectEvents<Events...> >
+{
+    static constexpr bool value = (_Count<E,Events...>::value > 0);
+    //we need not optimize that because the template instance already exists anyway;
+};
+
+template <typename E, typename NotSubjectEvents>
+struct IsSupportedEvent
+{
+    struct Wrong;
+    static_assert(std::is_same<NotSubjectEvents,Wrong>::value,
+        "USAGE: Observer::IsSupportedEvent<.,SubjectEvents<...>>");
+};
+
+
+//
+//
+//
+
+
+template <typename E, typename ... Events>
+struct Event< E, SubjectEvents<Events...> >
+{
+    static_assert(IsSupportedEvent<E,SubjectEvents<Events...>>::value,
+        "ERROR: Observer::Event<E,SubjectEvents<...>>: 'E' is not in the SubjectEvents list");
+};
+template <typename E, typename NotSubjectEvents>
+struct Event
+{
+    struct Wrong;
+    static_assert(std::is_same<NotSubjectEvents,Wrong>::value,
+        "USAGE: Observer::Event<.,SubjectEvents<...>>");
+};
+
+
+//
+//
+//
+
+
+template <typename Event, typename _SubjectEvents>
+struct EventIndex;
+
+template <typename Event, typename hEvent, typename ... rEvents>
+struct EventIndex< Event, SubjectEvents<hEvent,rEvents...> >
+{ static constexpr size_t value = 1+EventIndex< Event,SubjectEvents<rEvents...> >::value; };
+
+template <typename Event, typename ... rEvents>
+struct EventIndex< Event, SubjectEvents<Event,rEvents...> >
+{ static constexpr size_t value = 0; };
 
 
 }//close Observer
--- tests/test_observer.cpp
+++ tests/test_observer.cpp
@@ -3,3 +3,104 @@
 #include "../observer.hpp"
 
 
+TEST_CASE("Testing _Count") {
+    
+    //_Count<typename E, typename ... List>
+    struct E;
+    CHECK(Observer::_Count<E>::value == 0);
+    CHECK(Observer::_Count<E,E>::value == 1);
+    CHECK(Observer::_Count<E,E,E>::value == 2);
+    struct A;
+    CHECK(Observer::_Count<E,A>::value == 0);
+    CHECK(Observer::_Count<E,A,E>::value == 1);
+    CHECK(Observer::_Count<E,E,A>::value == 1);
+    CHECK(Observer::_Count<E,A,E,E>::value == 2);
+    CHECK(Observer::_Count<E,A,E,A>::value == 1);
+    CHECK(Observer::_Count<E,A,E,A,E>::value == 2);
+
+}
+
+
+TEST_CASE("Testing _EachOnce") {
+    
+    //_EachOnce<typename ...>
+    CHECK(Observer::_EachOnce<>::value == true);
+    struct A;
+    CHECK(Observer::_EachOnce<A>::value == true);
+    CHECK(Observer::_EachOnce<A,A>::value == false);
+    CHECK(Observer::_EachOnce<A,A,A>::value == false);
+    struct B;
+    CHECK(Observer::_EachOnce<A,B>::value == true);
+    CHECK(Observer::_EachOnce<A,B,A>::value == false);
+
+}
+
+
+TEST_CASE("Testing _ForEach") {
+
+    //_ForEach<template <typename> class Pred, typename ... >
+    CHECK(Observer::_ForEach<std::is_pointer>::value == true);
+    struct A;
+    CHECK(Observer::_ForEach<std::is_pointer,A>::value == false);
+    CHECK(Observer::_ForEach<std::is_pointer,A*>::value == true);
+    CHECK(Observer::_ForEach<std::is_pointer,A*,A>::value == false);
+    CHECK(Observer::_ForEach<std::is_pointer,A*,A*>::value == true);
+
+}
+
+
+TEST_CASE("Testing _IsDefined") {
+
+    //_IsDefined<typename T>
+    struct A;
+    CHECK(Observer::_IsDefined<A>::value == false);
+    struct B {};
+    CHECK(Observer::_IsDefined<B>::value == true);
+
+}
+
+
+TEST_CASE("Testing SubjectEvents") {
+
+    //SubjectEvents<typename ... Events>
+    
+    struct A;
+    CHECK(Observer::SubjectEvents<A,A>::AssertNoDuplicate() == false);
+    struct B;
+    CHECK(Observer::SubjectEvents<A,B>::AssertNoDuplicate() == true);
+   
+    CHECK(Observer::SubjectEvents<A>::AssertAllDefined() == false);
+    struct C {};
+    CHECK(Observer::SubjectEvents<C>::AssertAllDefined() == true);
+    CHECK(Observer::SubjectEvents<C,A>::AssertAllDefined() == false);
+    CHECK(Observer::SubjectEvents<C,C>::AssertAllDefined() == true);
+
+}
+
+
+TEST_CASE("Testing IsSupportedEvent") {
+
+    //IsSupportedEvent<typename E, typename _SubjectEvents>
+    struct A {};
+    struct B {};
+    using events_t = Observer::SubjectEvents<A,B>; 
+    struct C {};
+
+    CHECK(Observer::IsSupportedEvent<A,events_t>::value == true);
+    CHECK(Observer::IsSupportedEvent<B,events_t>::value == true);
+    CHECK(Observer::IsSupportedEvent<C,events_t>::value == false);
+
+}
+
+
+TEST_CASE("Testing EventIndex") {
+
+    //EventIndex<typename E, typename _SubjectEvents>
+    struct A {};
+    struct B {};
+    using events_t = Observer::SubjectEvents<A,B>; 
+    
+    CHECK(Observer::EventIndex<A,events_t>::value == 0);
+    CHECK(Observer::EventIndex<B,events_t>::value == 1);
+
+}
