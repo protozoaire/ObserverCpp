59c8c0e512411655582be7cc7b4e6a19457200ae
Vivien Dubois <vivien@voidetoile.fr>
2022-01-11 14:10:47 +0100

    Basic setup
    - included copy of doctest
    - run_tests script
    - blank observer.hpp

---
+++ observer.hpp
@@ -0,0 +1,28 @@
+#ifndef OBSERVER_HPP
+  #define OBSERVER_HPP 0
+  #define OBSERVER_CTX
+////////////////////////////
+
+
+#include <cstddef>
+
+
+namespace Observer {
+
+
+
+
+
+
+}//close Observer
+
+
+////////////////////////////
+#undef OBSERVER_CTX
+#undef OBSERVER_HPP
+  #define OBSERVER_HPP 1
+#else
+  #if OBSERVER_HPP == 0
+  #error incomplete include of "observer.hpp"
+  #endif
+#endif
